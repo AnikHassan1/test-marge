bb584271adcd287ff32f4ce74d0ede4c1884e674
Anik Hassan1 <ah491606@gmail.com>
2023-10-01 19:23:40 +0600

    test-marge

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// var i = 1;
+// var sum = 0;
+// while (i <= 5) {
+
+//     sum = sum + 1;
+//     i = i + 1;
+//     document.write("<br>",i);
+// }
+// document.write(sum);
+// for (var i = 1; i <= 10; i = i + 2) {
+//     document.write("<br>", i)
+// }
+var i=1;
+do{
+    document.write(i);
+    i++;
+}
+while(i<=10)
+
