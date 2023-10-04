c9d8c4e62918e6c569872adc8eef4196bb6c43b5
Anik Hassan1 <ah491606@gmail.com>
2023-10-04 07:52:21 +0600

    first

--- js/index.js
+++ js/index.js
@@ -10,10 +10,21 @@
 // for (var i = 1; i <= 10; i = i + 2) {
 //     document.write("<br>", i)
 // }
-var i=1;
-do{
-    document.write(i);
-    i++;
-}
-while(i<=10)
+// var i=1;
+// do{
+//     document.write(i);
+//     i++;
+// }
+// while(i<=10)
+// var i=0;
+// do{
+// document.write(i);
+// i++;
+// }while(i<=100);
 
+let x="        anik            ";
+let y=x.trim();
+
+// let z = x.concat(" ",y);
+document.write(x.length)
+document.write(y.length)
